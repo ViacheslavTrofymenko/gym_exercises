3226fc881fb02968d00a3c42b8bc7a74d4eae83a
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2023-01-18 13:51:59 +0200

    added navigation component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,5 @@
-import { Link } from 'react-router-dom';
-import { Stack } from '@mui/material';
-
-import Logo from '../assets/images/Logo.png';
-
 const Home = () => {
-  return (
-    <Stack>
-      <Link to="/">
-        <img
-          src={Logo}
-          alt="logo"
-          style={{ width: '48px', height: '48px', margin: '0 20px' }}
-        />
-      </Link>
-      <Stack>
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          Home
-        </Link>
-      </Stack>
-    </Stack>
-  );
+  return <div> Home</div>;
 };
 
 export default Home;
