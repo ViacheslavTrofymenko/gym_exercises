a2c7bd6969bbb09543afa2c69415a4ce455690b6
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2023-01-20 11:39:48 +0200

    completed home page with it components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,22 @@ import { Box } from '@mui/material';
 import { HeroBanner, SearchExercises, Exercises } from '../components';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
